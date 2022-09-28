d28fd81e87a4265e065874131322aa1a5cd9ed3e
Suzan-Tulinde <59966248+Agaba-Ed@users.noreply.github.com>
2022-09-28 19:03:56 +0300

    Create models and routes

---
+++ app/Models/ClientDetail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientDetail extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->hasOne(User::class, 'client_id','client_id');
+    }
+
+    public function client_attachments(){
+        return $this->hasMany(ClientAttachment::class, 'client_id','client_id');
+    }
+
+
+
+    
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'username',
+       ];
+       
+    protected $table = 'client_details';
+    protected $primaryKey = 'client_id';
+    public $timestamps = false;   
+}
---
+++ app/Models/LoanApplication.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoanApplication extends Model
+{
+    use HasFactory;
+
+    
+    protected $fillable = [
+        'user_id',
+        'loan_amount',
+        'payment_mode_id',
+        'mobile_money_no',
+        'no_of_months',
+        'mobile_money_name',
+        'approved',
+        'loan_number',
+    ];
+
+}
